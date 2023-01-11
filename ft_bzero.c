f7247e3e9493df6804d33b5231d4126c87973e50
Jan Rosmarin <jrosmari@c2r9s3.42prague.com>
2023-01-11 20:13:06 +0100

    ft_bzero, ft_memset fixed

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,43 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 19:18:22 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/11 19:59:10 by jrosmari         ###   ########.fr       */
+/*   Created: 2023/01/11 19:51:34 by jrosmari          #+#    #+#             */
+/*   Updated: 2023/01/11 20:12:31 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
-void	*ft_memset(void *s, int c, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned long	i;
 
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)s)[i] = c;
+		((unsigned char *)s)[i] = 0;
 		i++;
 	}
-	return (s);
 }
 /*
 #include <stdio.h>
 
 int	main(void)
 {
-	int	i = 8;
-	char	c = '@';
+	int i = 5;	
 
 	char	str[] = "This is a testing string";
 	printf("Printing testing string [%s]\n", str);
-	printf("Setting char [%c] %dx to string \n", c, i);
-	ft_memset(str, c, i);
+	printf("Setting char [\\0] %dx to string \n", i);
+	ft_bzero(str, i);
 	printf("Printing string after memset [%s]", str);
 	return (0);
 }
