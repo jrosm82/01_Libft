c018e86657e3ae275584b94dc9bcb74d03b999e3
Jan Rosmarin <jrosmari@c2r9s3.42prague.com>
2023-01-12 17:42:01 +0100

    ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 17:07:07 by jrosmari          #+#    #+#             */
+/*   Updated: 2023/01/12 17:20:00 by jrosmari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (s1[i] == s2[i] && i < n - 1 && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	const char str1[] = "string1";
+	const char str2[] = "atring1";
+	int size = 1;
+	
+	printf("strncmp size[%d] of str1[%s] and str2[%s] is
+	 [%d]\n", size, str1, str2, strncmp(str1, str2, size));
+	printf("ft_strncmp size[%d] of str1[%s] and str2[%s] is
+	 [%d]\n", size, str1, str2, ft_strncmp(str1, str2, size));
+
+
+
+
+	return (0);
+}*/
