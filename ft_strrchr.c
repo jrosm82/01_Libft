4a6d4ab4fbfb47e2ed41e2c906c9ccace921c447
Jan Rosmarin <jrosm82@gmail.com>
2023-01-15 16:32:30 +0100

    strchr fixed, strrchr first version

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/15 12:45:08 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/15 16:30:12 by jrosmari         ###   ########.fr       */
+/*   Created: 2023/01/15 15:28:31 by jrosmari          #+#    #+#             */
+/*   Updated: 2023/01/15 16:31:56 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
-char	*ft_strchr(const char *s, int c)
-{		
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
 	while (*s != '\0')
-	{
+	{		
+		i++;
+		s++;		
+	}
+	s--,
+	i--;	
+	while (i > -1)
+	{		
 		if (*s == c)
 			return ((char *)s);
-		s++;
+		s--;
+		i--;
 	}
 	return (NULL);
 }
@@ -29,20 +40,18 @@ char	*ft_strchr(const char *s, int c)
 
 int	main(void)
 {
-	const char str[] = "String to find String to find";
+	const char str[] = "String to test String to test";
 	char c = 'S';
 
-	char *occ = strchr(str,c);
-	printf("strchr function\n");
-	printf("printing string [%s] with first occurence of char [%c]\n",str, c);
-	printf("result = [%s]\n\n", occ);
-
-	printf("ft_strchr function\n");
-	printf("printing string [%s] with first occurence of char [%c]\n",str, c);
-	char *ft_occ = ft_strchr(str,c);
+	char *ft_occ = ft_strrchr(str,c);
+	printf("ft_strrchr function\n");
+	printf("printing string [%s] with last occurence of char [%c]\n",str, c);	
 	printf("result = [%s]\n\n", ft_occ);
-	if (ft_occ == NULL)
-		printf("NULL");
+
+	char *occ = strrchr(str,c);
+	printf("strchr function\n");
+	printf("printing string [%s] with last occurence of char [%c]\n",str, c);
+	printf("result = [%s]\n\n", occ);	
 
 	return (0);
 }*/
