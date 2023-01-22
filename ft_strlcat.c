4e46c67a27b02e4663630d24841ca3363399e2dd
Jan Rosmarin <jrosm82@gmail.com>
2023-01-22 08:14:04 +0100

    ft_strlcat fixed errors

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 21:24:46 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/22 07:23:29 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/22 08:12:50 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	srclen;
+	size_t	destlen;
 	size_t	i;
 
 	srclen = 0;
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[srclen] != '\0' && srclen < size - 1)
-	{
-		dest[i] = src[srclen];
+	destlen = 0;
+	while (src[srclen] != '\0')
 		srclen++;
+	if (size == 0)
+		return (srclen);
+	while (dest[destlen] != '\0' && destlen < size)
+		destlen++;
+	if (destlen == size)
+		return (srclen + size);
+	i = destlen;
+	while (src[i - destlen] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i - destlen];
 		i++;
 	}
 	dest[i]	= '\0';
-	return (srclen);
-}
-
-int	main(void)
-{
-
-	return (0);
+	return (destlen + srclen);
 }
