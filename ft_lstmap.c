a407eca6f9cf467c85f58db82d8127ecac788af8
Jan Rosmarin <jrosm82@gmail.com>
2023-01-25 15:26:20 +0100

    FINAL COMMIT ALL PASSED

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:12:24 by jrosmari          #+#    #+#             */
-/*   Updated: 2023/01/25 09:11:05 by jrosmari         ###   ########.fr       */
+/*   Updated: 2023/01/25 15:23:39 by jrosmari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
 	t_list	*actual;
-	
-	if (!lst)
-		return NULL;
-	actual = ft_lstnew(f(lst->content));
-	del(lst->content);		
+	t_list	*new;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	actual = new;
 	lst = lst->next;
-	new = actual;	
 	while (lst != NULL)
 	{
-		actual->next = ft_lstnew(f(lst->content));			
+		actual->next = ft_lstnew(f(lst->content));
+		if (!actual)
+			del(lst);
 		actual = actual->next;
-		del(lst->content);
-		lst = lst->next;			
+		lst = lst->next;
 	}
 	return (new);
 }
